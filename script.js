406313234484c7b08ef71654720659fbf4e8e332
sharath <sharathsanachandra@gmail.com>
2024-01-20 19:28:17 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,50 @@
+let arr = [
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+];
+
+// Don't worry about consoling these functions, they are already being called on the button clicks.
+// Please don't change anything in the index.html file.
+
+function PrintDeveloper() {
+  //Write your code here , just console.log
+  const developers = arr.filter(employee => employee.profession === 'developer');
+  console.log('Developers:', developers);
+}
+
+function addData() {
+  //Write your code here, just console.log
+  const newEmployee = { id: 4, name: 'susan', age: '20', profession: 'intern' };
+  arr.push(newEmployee);
+  console.log('New Employee Added:', newEmployee);
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+  arr = arr.filter(employee => employee.profession !== 'admin');
+  console.log('Admin Removed:', arr);
+
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+  const newArray = [
+    { id: 5, name: 'alice', age: '22', profession: 'developer' },
+    { id: 6, name: 'bob', age: '21', profession: 'intern' },
+  ];
+
+  // Concatenate the new array with the original array.
+  arr = arr.concat(newArray);
+  console.log('Arrays Concatenated:', arr);
+}
+
+// Here is an example of how functions work,
+// basically a function is a block of code which can directly access your 'arr' variable since arr is global.
+// If I have a function called consoleArr(), that can directly access arr like this to console it.
+
+function consoleArr() {
+  console.log('Consoling Array Variable', arr);
+  // Over here I can directly access the variable.
+  arr.forEach((element) => console.table(element));
+}
